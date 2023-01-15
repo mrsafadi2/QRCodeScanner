515d7f3c853207c61bb2fddd7856a7052a064727
mrsafadi2 <mrsafadi2@gmail.com>
2023-01-15 17:59:49 +0200

    Initial Commit

---
+++ QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by Mohammed Safadi Macbook Pro on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
