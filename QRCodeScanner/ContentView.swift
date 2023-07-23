768a5d931ef9d726a7dd991d4c376bd8329c100c
Mohammed Safadi <119953152+mrsafadi2@users.noreply.github.com>
2023-07-23 13:02:15 +0300

    last update

--- QRCodeScanner/ContentView.swift
+++ QRCodeScanner/ContentView.swift
@@ -6,18 +6,69 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    @State private var isShowingScanner = false
+    @State private var scanResult:String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Text("Scan Will be here")
+            VStack {
+                VStack {
+                    Text("Scan QR Code Example")
+                        .font(.subheadline)
+                    
+                    if scanResult !=  "" {
+                        Text("Scan result :  \(scanResult)")
+                            .font(.headline)
+                    }
+
+                 }
+                .padding(.vertical, 20)
+                
+                Spacer()
+                HStack {
+                    Button(action: {
+                        isShowingScanner.toggle()
+                        
+                    }, label: {
+                        Image(systemName: isShowingScanner ? "bolt.fill" : "bolt.slash.fill")
+                            .imageScale(.large)
+                            .foregroundColor(isShowingScanner ? Color.yellow : Color.blue)
+                            .padding()
+                    })
+                }
+                .background(Color.white)
+                .cornerRadius(10)
+                
+            }.padding()
+            
+            
         }
-        .padding()
+        .sheet(isPresented: $isShowingScanner) {
+            CodeScannerView(codeTypes: [.qr], simulatedData: "40", completion: handleScan)
+        }
     }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+       isShowingScanner = false
+        switch result {
+        case .success(let result):
+            let details = result.string
+            self.scanResult = details
+            print(details)
+ 
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+
+    
 }
+
+
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
